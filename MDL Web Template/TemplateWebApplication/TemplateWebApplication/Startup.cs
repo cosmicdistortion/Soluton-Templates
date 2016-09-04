f39507acd1ee4fdda03e7baadc1ec8f81b7f384f
Michael Gordon <cosmic.mick@gmail.com>
2016-09-04 19:22:50 +1000

    Adding first draft of MDL web application template

---
+++ MDL Web Template/TemplateWebApplication/TemplateWebApplication/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TemplateWebApplication.Startup))]
+namespace TemplateWebApplication
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
